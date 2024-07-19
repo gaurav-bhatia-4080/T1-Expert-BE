3f02417b51cd4a210d0516865e41ce6635fdee42
Yash12Bansal <yashbansal1011@gmail.com>
2024-07-20 01:13:33 +0530

    New Doctor Approval api  added with secure true

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(
     resave: false,
     secret: "secret",
     cookie: {
-      secure: false,
+      secure: true,
       maxAge: 2 * 24 * 60 * 60 * 1000,
       sameSite: "none",
       path: "/",
